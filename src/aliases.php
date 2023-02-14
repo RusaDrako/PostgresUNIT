d06a34708b0a9a154dacad3f6f838c74d009cc2c
Leonid Petukhov <32844979+RusaDrako@users.noreply.github.com>
2023-02-14 10:34:50 +0300

    Update aliases.php

--- src/aliases.php
+++ src/aliases.php
@@ -3,9 +3,9 @@
 if (class_exists('PG_UNIT', false)) { return; }
 
 $classMap = [
-	'RusaDrako\\pgunit\\testSQL'            => 'PG_UNIT_Test',
-	'RusaDrako\\pgunit\\testSQLfunctions'   => 'PG_UNIT_Test_Functions',
-	'RusaDrako\\pgunit\\testSQLtriggers'    => 'PG_UNIT_Test_Triggers',
+	'RusaDrako\\pgunit\\testSQL'            => 'PGUNIT_Test',
+	'RusaDrako\\pgunit\\testSQLfunctions'   => 'PGUNIT_Test_Functions',
+	'RusaDrako\\pgunit\\testSQLtriggers'    => 'PGUNIT_Test_Triggers',
 ];
 
 foreach ($classMap as $class => $alias) {
